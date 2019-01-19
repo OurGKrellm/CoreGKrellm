016071dcf771585762445b4c64840fda9787675a
Lucas ALLOIN <lucas.alloin@epitech.eu>
2019-01-19 11:23:03 +0100

    add UserInformation file

---
+++ inc/UserInformation.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2019
+** cpp_rush3_2018
+** File description:
+** UserInformation.hpp
+*/
+
+#ifndef CPP_RUSH3_2018_USERINFORMATION_HPP
+#define CPP_RUSH3_2018_USERINFORMATION_HPP
+
+#include "IMonitorModule.hpp"
+
+class UserInformation : public IMonitorModule
+{
+
+};
+
+#endif //CPP_RUSH3_2018_USERINFORMATION_HPP
---
+++ src/modules/user_information/UserInformation.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2019
+** cpp_rush3_2018
+** File description:
+** UserInformation.cpp
+*/
+
+#include "UserInformation.hpp"
